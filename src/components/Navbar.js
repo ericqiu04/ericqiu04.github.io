b33abee359a5631243df513db51e76cd4ae31879
Eric Qiu <104801983+ericqiu04@users.noreply.github.com>
2023-01-03 13:34:22 -0500

    working dark mode

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,20 @@
-import {Container, Icon} from '@chakra-ui/react'
+import {Icon} from '@chakra-ui/react'
 import {FaGithub, FaLinkedin} from 'react-icons/fa'
 import {GrDocumentPdf} from 'react-icons/gr'
-import { Stack, HStack, VStack } from '@chakra-ui/react'
+import { useColorMode, useColorModeValue} from '@chakra-ui/react'
+import { HStack} from '@chakra-ui/react'
 import { Box } from '@chakra-ui/react'
 const Navbar = () => {
-   
-   return(<Box maxW ='100%' p={4}>
+    const { toggleColorMode } = useColorMode()
+    const bg = useColorModeValue('white', 'black')
+    const color = useColorModeValue('black', 'white')
+
+   return(<Box maxW ='100%' p={4} bg = {bg} color = {color} >
+
         <HStack spacing = {8} justify = 'right' mr = {20} mt = {5}>
             <Icon as={FaGithub} boxSize = {10}/>
             <Icon as = {GrDocumentPdf} boxSize = {10}/>
-            <Icon as = {FaLinkedin} boxSize = {10}/>
+            <Icon as = {FaLinkedin} boxSize = {10} onClick = {toggleColorMode}/>
         </HStack>
     </Box>
    )
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { ColorModeScript } from '@chakra-ui/react';
+import theme from './theme';
 import { ChakraProvider } from '@chakra-ui/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ChakraProvider>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App/>
     </ChakraProvider>
   </React.StrictMode>
