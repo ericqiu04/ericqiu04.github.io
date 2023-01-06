b0488bf7259d3b38f6933eaa4011e5be6b51526b
Eric Qiu <104801983+ericqiu04@users.noreply.github.com>
2023-01-06 10:39:57 -0500

    added more to mobile nav

--- src/components/mobileNav.js
+++ src/components/mobileNav.js
@@ -1,14 +1,16 @@
-import { Box, Center, Flex, Text, Stack} from "@chakra-ui/react"
+import { Box,  Flex, Text, VStack} from "@chakra-ui/react"
 
 const MobileNav = () => {
+    var fontSize = 20;
     return (
 
-        <Box position="fixed" right="0" h="100%" w="200px">
+        <Box position="fixed" right="0" h="100%" w="250px">
             <Flex position="fixed" h="100%" w="100%" textAlign="center">
-                <Stack w = "200px" mt = "50px">
-                    <Text display="block">Github</Text>
-                    <Text display="block">LinkedIn</Text>
-                </Stack>
+                <VStack spacing = {4}w = "250px" mt = "50px">
+                    <a  href="https://github.com/ericqiu04" target="blank"><Text fontSize = {fontSize} display="block">Github</Text></a>
+                    <a href="https://www.linkedin.com/in/eric-qiu-72115623b/" target="blank"><Text fontSize = {fontSize} display="block">LinkedIn</Text></a>
+                    <a href="#" target="blank"><Text fontSize = {fontSize} display="block">Resume</Text></a>
+                </VStack>
 
             </Flex>
         </Box>
